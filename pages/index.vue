5d5c1919c099b03471f6d2f9bc3bbf59d1058cbf
Anmol Goyal <anmolgoyal@gmail.com>
2021-01-01 23:06:43 +0530

    updated LandingPage

--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,312 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        GenZSwap
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
+  <div class="light">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+    <!-- Plugins -->
+    <link rel="stylesheet" href="/LandingPage/css/flipclock.css">
+    <link rel="stylesheet" href="/LandingPage/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="/LandingPage/css/owl.theme.default.min.css">
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="/LandingPage/css/dark.css">
+
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">
+            <img src="/LandingPage/images/coindash-logo-light.svg" alt="logo">
         </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Use Cases</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Stable Coin Distribution </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">RoadMap</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="site-header  d-flex flex-column align-items-center justify-content-between">
+        <div class="hero">
+            <h1>The
+                <span class="highlight">Digital Asset</span> for Payments</h1>
+            <p class="lead mt-3 mx-auto">It’s the fastest and most scalable digital asset, enabling real-time global
+                payments anywhere in the world.</p>
+            <a href="#" class="btn btn-primary mt-3">Explore Coin
+                <img src="/LandingPage/images/right-arrow.svg" alt="arrow">
+            </a>
+        </div>
+
+        <div class="graph">
+            <img src="/LandingPage/images/graph-light.svg" alt="graph" class="img-fluid">
+        </div>
+
+        <div class="text-center mx-4">
+            <h4 class="centered-text mb-4 text-muted">Built for enterprise use, Coin offers banks and payment providers
+                a reliable, on-demand option to source liquidity
+                for cross-border payments.</h4>
+            <a href="#" class="btn btn-outline-secondary">View Features</a>
+        </div>
+
     </div>
+
+    <section class="section feature-highlight">
+        <div class="container">
+            <div class="section-title mini-title">
+                <h4>Coin Everywhere</h4>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-md-4">
+                    <div class="feature-item">
+                        <img src="/LandingPage/images/feature-one.svg" alt="icon" class="featured-item-icon">
+                        <h4 class="featured-item-title">Banking Software</h4>
+                        <p>Using Coin, banks can source liquidity on demand in real time without having to pre-fund
+                            nostro accounts.</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="feature-item">
+                        <img src="/LandingPage/images/feature-two.svg" alt="icon" class="featured-item-icon">
+                        <h4 class="featured-item-title">Payment Providers</h4>
+                        <p>Payment Providers use Coin to lower foreign exchange costs and provide faster payment
+                            settlement.</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="feature-item">
+                        <img src="/LandingPage/images/feature-three.svg" alt="icon" class="featured-item-icon">
+                        <h4 class="featured-item-title">Digital Agreements</h4>
+                        <p>It’s the fastest and most scalable digital asset, enabling real-time global payments
+                            anywhere in
+                            the world.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- // container -->
+    </section>
+
+    <section class="section section-highlight">
+        <div class="container">
+
+
+            <div class="section-title max-title">
+                <h3>Coindash Token Sale</h3>
+            </div>
+            <p class="lead p-3 centered-text text-center mx-auto">Coindash Token Pool is a fixed token economy set by
+                the demand of the Coin Token Main Sale, which concluded on
+                May 20</p>
+
+            <div class="text-center">
+                <div class="countdown-clock" data-datetime="January 28, 2019 10:30:00"></div>
+            </div>
+
+            <div class="progress-area">
+                <div class="row">
+                    <div class="col-2 col-md-1 text-right">
+                        <span> $0 </span>
+                    </div>
+                    <div class="col-8 col-md-10">
+                        <div class="progress">
+                            <div class="progress-bar bg-gradient" role="progressbar" style="width: 75%" aria-valuenow="75"
+                                aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                    </div>
+                    <div class="col-2 col-md-1 text-left">
+                        <span> $5M </span>
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- // progress-area -->
+
+            <div class="row text-center">
+                <div class="col">
+                    <h6 class="text-info">
+                        POOL SUPPLY
+                    </h6>
+                    <h3 class="display-5 mb-5">14,325,000,0000</h3>
+                </div>
+                <div class="col">
+                    <h6 class="text-info">
+                        AMOUNT LEFT NOW
+                    </h6>
+                    <h3 class="display-5 mb-5">50,000,000,000</h3>
+
+                </div>
+                <div class="col">
+                    <h6 class="text-info">
+                        POOL SUPPLY
+                    </h6>
+                    <h3 class="display-5 mb-5">38,525,215,700</h3>
+
+                </div>
+            </div>
+            <!-- // row -->
+            <div class="text-center">
+                <div class="btn btn-lg btn-primary mt-5">Purchase Coin Now
+                    <img src="/LandingPage/images/right-arrow.svg" alt="arrow">
+                </div>
+                <div class="text-muted my-3">
+                    Limited supply: after sale ends, coin obtained only through mining & exchanges
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12 col-md-6 offset-md-4">
+                    <div class="section-title mini-title left-aligned">
+                        <h4>Stable Coin Distribution</h4>
+                    </div>
+                    <div class="d-flex flex-column flex-lg-row justify-content-between align-items-center my-5 mb-md-0">
+                        <div class="flex-fill flex-shrink-1 w-50">
+                            <canvas id="distChart" class="donut-chart"></canvas>
+                        </div>
+                        <div id="dist_legend" class="flex-fill w-50 flex-grow-1 donut-legend"></div>
+                    </div>
+                </div>
+            </div>
+            <!-- // row -->
+
+        </div>
+        <!-- // container -->
+    </section>
+    <!-- // section -->
+
+    <section class="section">
+        <div class="container">
+            <div class="row align-items-center justify-content-between mt-5">
+                <div class="col-12 col-md-6">
+                    <img src="/LandingPage/images/checkout-illustration-light.jpg" alt="illustration" class="img-fluid">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h6 class="text-info mb-4">
+                        WHY COINDASH
+                    </h6>
+                    <h2 class="mb-4">Together, We’re Modernizing Global Payments</h2>
+                    <p class="lead mb-4">
+                        It’s the fastest and most scalable digital asset, enabling real-time global payments anywhere
+                        in the world.
+                    </p>
+
+                    <p>
+                        Today the world sends more than $155 trillion* across borders. Yet, the underlying
+                        infrastructure is dated and flawed. Coindash
+                        connects banks, payment providers and digital asset exchanges via RippleNet to provide one
+                        frictionless
+                        experience to send money globally.</p>
+
+                    <a href="#" class="btn btn-primary mt-3">Read our Mission</a>
+
+                </div>
+            </div>
+
+        </div>
+        <!-- // container -->
+    </section>
+    <!-- // section -->
+
+    <section class="section">
+        <div class="container">
+
+
+            <div class="section-title max-title">
+                <h3>Project Roadmap</h3>
+            </div>
+            <p class="lead p-3 centered-text text-center mx-auto">The roadmap focuses on the development of the
+               project with respect to financial knowledge, Smart Contract Development and User interface development.
+            </p>
+
+
+            <section class="cd-timeline js-cd-timeline">
+                <div class="cd-timeline__container">
+                    <div class="cd-timeline__block js-cd-block">
+                        <div class="cd-timeline__milestone"> </div>
+                        <div class="cd-timeline__content js-cd-content">
+                            <div class="cd-timeline__date">Saturday</div>
+                            <h4>Project Concept</h4>
+                            <p> Understanding Aave Whitepaper and studying CDP ( Collateralized Debt Position) </p>
+                        </div>
+                    </div>
+                    <div class="cd-timeline__block js-cd-block">
+                        <div class="cd-timeline__milestone"> </div>
+                        <div class="cd-timeline__content js-cd-content">
+                            <div class="cd-timeline__date">Sunday</div>
+                            <h4>StableCoin Development</h4>
+                            <p>Writing Smart Contracts for StableCoins.</p>
+                        </div>
+                    </div>
+                    <div class="cd-timeline__block js-cd-block">
+                        <div class="cd-timeline__milestone"> </div>
+                        <div class="cd-timeline__content js-cd-content">
+                            <div class="cd-timeline__date">Monday</div>
+                            <h4>StableCoin testing</h4>
+                            <p> Writing and executing different simulations for testing.</p>
+                        </div>
+                    </div>
+                    <div class="cd-timeline__block js-cd-block">
+                        <div class="cd-timeline__milestone"> </div>
+                        <div class="cd-timeline__content js-cd-content">
+                            <div class="cd-timeline__date">Tuesday</div>
+                            <h4>Lending Smart Contract Development </h4>
+                            <p>Writing Pool based Lending Protocol.</p>
+                        </div>
+                    </div>
+                    <div class="cd-timeline__block js-cd-block">
+                        <div class="cd-timeline__milestone"> </div>
+                        <div class="cd-timeline__content js-cd-content">
+                            <div class="cd-timeline__date">Wednesday</div>
+                            <h4>Web App Development</h4>
+                            <p>Coindash Mobile App is released and its avilable in iOS and Android devices.</p>
+                        </div>
+                    </div>
+                    <div class="cd-timeline__block js-cd-block">
+                        <div class="cd-timeline__milestone"> </div>
+                        <div class="cd-timeline__content js-cd-content">
+                            <div class="cd-timeline__date">Now</div>
+                            <h4 class="m-0">Web App Deployment</h4>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </div>
+        <!-- // container -->
+    </section>
+    <!-- // section -->
+  
+    <!-- JavaScript -->
+    <!-- jQuery & Bootstrap -->
+    <script src="/LandingPage/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="/LandingPage/js/bootstrap.bundle.min.js"></script>
+    <!-- Plugin JS -->
+    <script src="/LandingPage/js/plugins/flipclock.min.js"></script>
+    <script src="/LandingPage/js/plugins/chart.min.js"></script>
+    <script src="/LandingPage/js/plugins/owl.carousel.min.js"></script>
+    <!-- Custom JS -->
+    <script src="/LandingPage/js/script.js"></script>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
-export default Vue.extend({})
+export default Vue.extend({
+	head(){
+		return {
+			title : "GenZSwap"
+		}
+	}
+})
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
